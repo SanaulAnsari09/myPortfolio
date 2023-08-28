12acc24abc28d70503ef9051cf997f584e839f0b
sanaul ansari <ansarcode09@gmail.com>
2023-08-28 13:38:20 +0530

    i remove exat path

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App=()=>{
     <BrowserRouter>
         <Navbar/>
         <Routes>
-            <Route exat path='/' element={<Home/>}></Route>
+            <Route path='/' element={<Home/>}></Route>
             <Route path='/about' element={<About/>}></Route>
             <Route path='/project' element={<Project/>}></Route>
             <Route path='/contact' element={<Contact/>}></Route>

3ab47cad7167503d371cf08cf056a153b37e5835
sanaul ansari <ansarcode09@gmail.com>
2023-08-28 14:09:40 +0530

    ninth commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Home from './Components/Home';
-import {  Routes, Route, BrowserRouter } from 'react-router-dom';
+import {  Routes, Route, BrowserRouter, StrictMode } from 'react-router-dom';
 import About from './About/About';
 import Project from "./ProjectNav/Project";
 import Contact from './Contact/Contact';
@@ -10,16 +10,18 @@ import './App.css';
 const App=()=>{
   return(
     <>
-    <BrowserRouter basename={'/'}>
-        <Navbar/>
-        <Routes>
-            <Route exat path='/' element={<Home/>}></Route>
-            <Route path='/about' element={<About/>}></Route>
-            <Route path='/project' element={<Project/>}></Route>
-            <Route path='/contact' element={<Contact/>}></Route>
-        </Routes>
-          <Footer/>
-    </BrowserRouter>
+    <React.StrictMode>
+          <BrowserRouter basename={'/'}>
+              <Navbar/>
+              <Routes>
+                  <Route exat path='/' element={<Home/>}></Route>
+                  <Route path='/about' element={<About/>}></Route>
+                  <Route path='/project' element={<Project/>}></Route>
+                  <Route path='/contact' element={<Contact/>}></Route>
+              </Routes>
+                <Footer/>
+          </BrowserRouter>
+    </React.StrictMode>
     </>
   )
 }
